e93fc8a5c09fae704f65286ea51d77f67a95442d
Leonardo G.T. da Conceicao <leonardogtc@gmail.com>
2026-02-27 05:22:24 -0300

    Criando um endpoint para recebimento de produto

---
+++ src/main/java/br/com/leonardogtc/produtosapi/controller/ProdutoController.java
@@ -0,0 +1,19 @@
+package br.com.leonardogtc.produtosapi.controller;
+
+import br.com.leonardogtc.produtosapi.model.Produto;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("produtos")
+public class ProdutoController {
+
+    @PostMapping
+    public Produto salvar(@RequestBody Produto produto) {
+
+        System.out.println("Salvando produto: " + produto);
+        return produto;
+    }
+}
